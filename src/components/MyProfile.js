752668909af573067d69b6c3870e8254250a7bee
HXgrowns <804156017@qq.com>
2020-08-12 13:18:15 +0800

    [huxiao]{add}: add form html and css

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,10 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+  state = {
+    name: '',
+    gender: 'Male',
+    description: '',
+    isRead: false,
+  }
+
+  
+
   render() {
     return (
-      <form></form>
+      <form className='form'>
+        <h1 className='form-title section'>My Prodile</h1>
+        <section className='section'>
+          <label className='lable' htmlFor='name'>Name</label>
+          <input className='input' name='name' type='text' placeholder='Your name'></input>
+        </section>
+
+        <section className='section'>
+          <label className='lable' htmlFor='gender'>Gender</label>
+          <select className='input' name="gender">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </section>
+
+        <section className='section'>
+          <label className='lable' htmlFor='description'>Description</label>
+          <textarea className='input textarea-input' name='description' type='textarai' placeholder='Description about yourself'></textarea>
+        </section>
+
+        <section className='section checkout-section'>
+          <input type='checkbox'></input>
+          I have read the terms of conduct
+        </section>
+
+        <section className='submit-section'>
+          <input className='submit' type='submit' name='submit' value='Submit'></input>
+        </section>
+      </form>
     );
   }
 }
