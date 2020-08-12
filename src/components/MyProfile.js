9d1e06ff74bc7945f096057e2c764aec7b538bea
HXgrowns <804156017@qq.com>
2020-08-12 13:54:02 +0800

    [huxiao]{add}: add form function

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -9,20 +9,47 @@ class MyProfile extends Component {
     isRead: false,
   }
 
-  
+  handleName = (event) => {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
+  handleGender = (event) => {
+    this.setState({
+      gender: event.target.value,
+    })
+  }
+
+  handleTextarea = (event) => {
+    this.setState({
+      description: event.target.value,
+    })
+  }
+
+  handleCheckout = (event) => {
+    this.setState({
+      isRead: event.target.checked,
+    })
+  }
+
+  submit = (event) => {
+    console.log(this.state);
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <form className='form'>
+      <form className='form' onSubmit={this.submit}>
         <h1 className='form-title section'>My Prodile</h1>
         <section className='section'>
           <label className='lable' htmlFor='name'>Name</label>
-          <input className='input' name='name' type='text' placeholder='Your name'></input>
+          <input className='input' name='name' type='text' placeholder='Your name' onChange={this.handleName}></input>
         </section>
 
         <section className='section'>
           <label className='lable' htmlFor='gender'>Gender</label>
-          <select className='input' name="gender">
+          <select className='input' name="gender" onChange={this.handleGender}>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
           </select>
@@ -30,16 +57,18 @@ class MyProfile extends Component {
 
         <section className='section'>
           <label className='lable' htmlFor='description'>Description</label>
-          <textarea className='input textarea-input' name='description' type='textarai' placeholder='Description about yourself'></textarea>
+          <textarea className='input textarea-input' name='description' type='textarai' placeholder='Description about yourself' 
+          onChange={this.handleTextarea}></textarea>
         </section>
 
         <section className='section checkout-section'>
-          <input type='checkbox'></input>
+          <input type='checkbox' onChange = {this.handleCheckout}></input>
           I have read the terms of conduct
         </section>
 
         <section className='submit-section'>
-          <input className='submit' type='submit' name='submit' value='Submit'></input>
+          <input className='submit' type='submit' name='submit' value='Submit'
+          disabled = {this.state.name === '' || this.state.description === '' || this.state.isRead === false}></input>
         </section>
       </form>
     );
